fbfb449e4c42a1db94f7df1ddc6c923a3f035e0f
yasiraffan <30703318+yasiraffan@users.noreply.github.com>
2017-08-07 00:37:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/article-one', function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));  
 });
+app.get('/article-2', function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));  
+});
+app.get('/article-3', function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));  
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
